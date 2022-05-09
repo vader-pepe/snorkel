f425e3d73e43e81206266844c0d38024d045d7c7
Ihsan Kurniawan <ikurniawanlubis@gmail.com>
2022-05-09 09:42:08 +0700

    change browser to puppeteer-cluster

--- fb-pre-login.js
+++ fb-pre-login.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 const prompt = require('prompt-sync')({ sigint: true });
 const path = require('path');
 const iPhone = puppeteer.devices['iPhone SE'];
-
+const _puppeteerCluster = require("puppeteer-cluster");
 
 const userDir = path.resolve('./userDataDir')
 const facebookPath = path.resolve('./facebook');
@@ -35,94 +35,103 @@ const login = async () => {
         const user = prompt('username: ');
         const pass = prompt('password: ');
 
-        const browser = await puppeteer.launch({
-            userDataDir: userDir,
+        const cluster = await _puppeteerCluster.Cluster.launch({
+            concurrency: _puppeteerCluster.Cluster.CONCURRENCY_CONTEXT,
+            maxConcurrency: 1,
+            puppeteerOptions: {
+                userDataDir: userDir,
+            },
+            monitor: true,
+            timeout: 300000,
         });
-        const page = await browser.newPage();
 
-        await page.emulate(iPhone);
+        await cluster.task(async ({ page, data = "https://www.facebook.com/" }) => {
+            await page.emulate(iPhone);
 
-        await page.goto('https://www.facebook.com', { waitUntil: 'networkidle2' });
+            await page.goto(data, { waitUntil: 'networkidle2' });
 
-        await page.waitForSelector("#m_login_email");
-        const isNotLoggedIn = await page.$("#m_login_email")
+            await page.waitForSelector("#m_login_email");
+            const isNotLoggedIn = await page.$("#m_login_email")
 
-        if (isNotLoggedIn) {
-            await page.type("#m_login_email", user) // login field
-            await page.type("#m_login_password", pass) // password field
-            await page.screenshot({ path: `${facebookPath}/2.png` })
-            // click Log In button
-            await Promise.all([page.click("button"), page.waitForNavigation({
-                waitUntil: 'networkidle2',
-                timeout: 0
-            })])
+            if (isNotLoggedIn) {
+                await page.type("#m_login_email", user) // login field
+                await page.type("#m_login_password", pass) // password field
+                await page.screenshot({ path: `${facebookPath}/2.png` })
+                // click Log In button
+                await Promise.all([page.click("button"), page.waitForNavigation({
+                    waitUntil: 'networkidle2',
+                    timeout: 0
+                })])
 
-            // check if you logged in or not
-            await page.screenshot({ path: "./facebook/3.png" })
-            const detectedForeignIP = await page.$("#checkpoint_title");
+                // check if you logged in or not
+                await page.screenshot({ path: "./facebook/3.png" })
+                const detectedForeignIP = await page.$("#checkpoint_title");
 
-            if (detectedForeignIP) {
-                await page.waitForSelector("#checkpointSubmitButton-actual-button");
-                await page.click("#checkpointSubmitButton-actual-button");
-                await delay(3000);
-
-                await page.screenshot({ path: "./facebook/4.png" })
-
-                const choices = await page.$$eval("fieldset > label", ele => ele.map(value => value));
-
-                await page.screenshot({ path: "./facebook/5.png" })
-
-                choices.map((value, index) => {
-                    console.log(`${index}: ${value.innerText}`);
-                })
-
-                const userChoice = prompt('Choose your choice: ');
-
-                if (!isNaN(userChoice)) {
-                    throw new Error("Must be a number!")
-                }
-
-                if (userChoice > (choices.length - 1)) {
-                    throw new Error("Not valid!");
-                }
-
-                await choices[userChoice].click();
-
-                if (choices[userChoice].innerText === "Provide your date of birth") {
-                    // date of birth choises here
-                } else if (choices[userChoice].innerText === "Get a code sent to your phone") {
-                    await page.click("input[type='submit']")
+                if (detectedForeignIP) {
+                    await page.waitForSelector("#checkpointSubmitButton-actual-button");
+                    await page.click("#checkpointSubmitButton-actual-button");
                     await delay(3000);
-                    await page.click("fieldset > label");
-                    await page.click("input[type='submit']")
-                    await delay(3000);
-                    const code = prompt("Please input your code: ");
 
-                    await page.type("#nonce_input_field", code);
+                    await page.screenshot({ path: "./facebook/4.png" })
 
-                    await page.click("input[type='submit']")
+                    const choices = await page.$$eval("fieldset > label", ele => ele.map(value => value));
 
-                } else if (choices[userChoice].innerText === "Approve your login on another computer") {
-                    await page.click("input[type='submit']")
-                    await delay(3000);
-                    await page.click("input[type='submit']")
-                    await delay(3000);
-                    await page.click("input[type='submit']")
-                }
+                    await page.screenshot({ path: "./facebook/5.png" })
 
-            } else {
-                // check if facebook wanted to save your password. this script simply press Not Now
-                const notNowEle = await page.$("a > span");
-                const notNow = await GetProperty(notNowEle, "textContent");
+                    choices.map((value, index) => {
+                        console.log(`${index}: ${value.innerText}`);
+                    })
 
-                await page.screenshot({ path: facebookPath + "/4.png" })
+                    const userChoice = prompt('Choose your choice: ');
 
-                if (notNow === "Not Now") {
-                    await page.click("a")
-                    await delay(3000)
+                    if (!isNaN(userChoice)) {
+                        throw new Error("Must be a number!")
+                    }
+
+                    if (userChoice > (choices.length - 1)) {
+                        throw new Error("Not valid!");
+                    }
+
+                    await choices[userChoice].click();
+
+                    if (choices[userChoice].innerText === "Provide your date of birth") {
+                        // date of birth choises here
+                    } else if (choices[userChoice].innerText === "Get a code sent to your phone") {
+                        await page.click("input[type='submit']")
+                        await delay(3000);
+                        await page.click("fieldset > label");
+                        await page.click("input[type='submit']")
+                        await delay(3000);
+                        const code = prompt("Please input your code: ");
+
+                        await page.type("#nonce_input_field", code);
+
+                        await page.click("input[type='submit']")
+
+                    } else if (choices[userChoice].innerText === "Approve your login on another computer") {
+                        await page.click("input[type='submit']")
+                        await delay(3000);
+                        await page.click("input[type='submit']")
+                        await delay(3000);
+                        await page.click("input[type='submit']")
+                    }
+
+                } else {
+                    // check if facebook wanted to save your password. this script simply press Not Now
+                    const notNowEle = await page.$("a > span");
+                    const notNow = await GetProperty(notNowEle, "textContent");
+
+                    await page.screenshot({ path: facebookPath + "/4.png" })
+
+                    if (notNow === "Not Now") {
+                        await page.click("a")
+                        await delay(3000)
+                    }
                 }
             }
-        }
+        });
+
+        await cluster.execute();
 
     } catch (error) {
         throw new Error(error);
--- instagram-pre-login.js
+++ instagram-pre-login.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const prompt = require('prompt-sync')({ sigint: true });
 const path = require('path');
 const iPhone = puppeteer.devices['iPhone SE'];
-
+const _puppeteerCluster = require("puppeteer-cluster");
 
 const userDir = path.resolve('./userDataDir')
 const instagramPath = path.resolve('./instagram');
@@ -18,79 +18,88 @@ function delay(time) {
 }
 
 const login = async () => {
-    try {
-        const user = prompt('username: ');
-        const pass = prompt('password: ');
-
-        const browser = await puppeteer.launch({
+    const cluster = await _puppeteerCluster.Cluster.launch({
+        concurrency: _puppeteerCluster.Cluster.CONCURRENCY_CONTEXT,
+        maxConcurrency: 1,
+        puppeteerOptions: {
             userDataDir: userDir,
+        },
+        monitor: true,
+        timeout: 300000,
+    });
+
+    try {
+        await cluster.task(async ({ page, data = "https://www.instagram.com/" }) => {
+            const user = prompt('username: ');
+            const pass = prompt('password: ');
+
+            await page.emulate(iPhone);
+
+            await page.goto(data);
+            await delay(3000);
+            await page.screenshot({ path: instagramPath + "/1.png" })
+            const findAllBtns = await page.$$eval("button", (elements) => elements.map((v) => v.textContent))
+            const isNotLoggedIn = findAllBtns.find(v => v === "Log In")
+
+            if (isNotLoggedIn) {
+
+                await page.screenshot({ path: instagramPath + "/2.png" })
+                await page.evaluate(() => {
+                    // eslint-disable-next-line no-undef
+                    const loginBtn = Array.from(document.querySelectorAll('button'))
+                        .find(el => el.textContent === `Log In`);
+
+                    loginBtn.click();
+                })
+
+                // login directly with username and password
+                await page.type("input[name='username']", user) // login field
+                await page.type("input[name='password']", pass) // password field
+                await page.screenshot({ path: instagramPath + "/3.png" })
+                // click Log In button
+                await delay(500)
+                await page.screenshot({ path: instagramPath + "/4.png" })
+                await Promise.all([
+                    page.click("button[type='submit']"),
+                    page.waitForNavigation({
+                        waitUntil: "networkidle2"
+                    })
+                ])
+                await delay(5000);
+                await page.evaluate(() => {
+                    // eslint-disable-next-line no-undef
+                    const x = Array.from(document.querySelectorAll("button"));
+
+                    x.map(v => {
+                        if (v.textContent === "Send Security Code") {
+                            v.click();
+                        }
+                    })
+                })
+                await delay(5000);
+                await page.screenshot({ path: instagramPath + "/5.png" })
+                const code = prompt("Code: ")
+
+                await page.type("#security_code", code);
+                await page.screenshot({ path: instagramPath + "/6.png" })
+
+                await page.evaluate(() => {
+                    // eslint-disable-next-line no-undef
+                    const x = Array.from(document.querySelectorAll("button"))
+
+                    x.map(v => {
+                        if (v.textContent === "Submit") {
+                            v.click();
+                        }
+                    })
+                })
+                await delay(5000);
+                await page.screenshot({ path: instagramPath + "/7.png" })
+
+            }
         });
-        const page = await browser.newPage();
 
-        await page.emulate(iPhone);
-
-        await page.goto("https://www.instagram.com/");
-        await delay(3000);
-        await page.screenshot({ path: instagramPath + "/1.png" })
-        const findAllBtns = await page.$$eval("button", (elements) => elements.map((v) => v.textContent))
-        const isNotLoggedIn = findAllBtns.find(v => v === "Log In")
-
-        if (isNotLoggedIn) {
-
-            await page.screenshot({ path: instagramPath + "/2.png" })
-            await page.evaluate(() => {
-                // eslint-disable-next-line no-undef
-                const loginBtn = Array.from(document.querySelectorAll('button'))
-                    .find(el => el.textContent === `Log In`);
-
-                loginBtn.click();
-            })
-
-            // login directly with username and password
-            await page.type("input[name='username']", user) // login field
-            await page.type("input[name='password']", pass) // password field
-            await page.screenshot({ path: instagramPath + "/3.png" })
-            // click Log In button
-            await delay(500)
-            await page.screenshot({ path: instagramPath + "/4.png" })
-            await Promise.all([
-                page.click("button[type='submit']"),
-                page.waitForNavigation({
-                    waitUntil: "networkidle2"
-                })
-            ])
-            await delay(5000);
-            await page.evaluate(() => {
-                // eslint-disable-next-line no-undef
-                const x = Array.from(document.querySelectorAll("button"));
-
-                x.map(v => {
-                    if (v.textContent === "Send Security Code") {
-                        v.click();
-                    }
-                })
-            })
-            await delay(5000);
-            await page.screenshot({ path: instagramPath + "/5.png" })
-            const code = prompt("Code: ")
-
-            await page.type("#security_code", code);
-            await page.screenshot({ path: instagramPath + "/6.png" })
-
-            await page.evaluate(() => {
-                // eslint-disable-next-line no-undef
-                const x = Array.from(document.querySelectorAll("button"))
-
-                x.map(v => {
-                    if (v.textContent === "Submit") {
-                        v.click();
-                    }
-                })
-            })
-            await delay(5000);
-            await page.screenshot({ path: instagramPath + "/7.png" })
-
-        }
+        await cluster.execute();
 
     } catch (error) {
         throw new Error(error.message);
