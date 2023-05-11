e749025e00041dc466ec6dcda68d48295298302e
chadwithz <kepoya@email.com>
2023-05-11 23:47:52 +0700

    feat: security code handling [WIP]

--- snorkel-frontend/src/pages/index.tsx
+++ snorkel-frontend/src/pages/index.tsx
@@ -1,31 +1,51 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { useEffect, useState } from 'react'
-import { Socket, io } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [isSecurityCodeFound, setIsSecurityCodeFound] = useState(false);
+  const [securityCode, setSecurityCode] = useState('');
+  const [socket, setSocket] = useState<Socket>();
 
   useEffect(() => {
     async function getInstance() {
-      const ioInstance = io('http://localhost:5000')
-      ioInstance.emit("welcome", "hello from FE")
+      const x = io('http://localhost:5000')
+      setSocket(x)
     }
     getInstance()
     return () => {
-
     }
   }, [])
+
+  useEffect(() => {
+    if (!!socket) {
+      console.log('Socket Connected')
+    }
+  }, [socket])
+
+  socket?.on('instagram-security-code', () => {
+    setIsSecurityCodeFound(true)
+  })
+
+  function submitHandler() {
+    socket?.emit('instagram-security-code-input', securityCode)
+  }
 
   return (
     <main
       className={`flex flex-col items-center justify-between p-24 ${inter.className} max-h-[350px] min-h-[300px]`}
     >
-      <input type='text' placeholder='Username' />
-      <input type='password' placeholder='Password' />
-      <input type='password' placeholder='Security Code' />
-      <button type='submit' >Submit</button>
+      <form className='flex flex-col gap-2' onSubmit={e => {
+        e.preventDefault();
+        submitHandler()
+      }}>
+        <input type='text' placeholder='Username' />
+        <input type='password' placeholder='Password' />
+        {isSecurityCodeFound ? <input type='password' onChange={e => setSecurityCode(e.target.value)} placeholder='Security Code' /> : null}
+        <button type='submit' >Submit</button>
+      </form>
     </main>
   )
 }
--- snorkel-service/src/instagram/controller/index.ts
+++ snorkel-service/src/instagram/controller/index.ts
@@ -2,61 +2,69 @@ import { Request, Response } from "express";
 import { page as mainPage, io } from "../..";
 import { instagramSelectors } from "../../constants";
 import { KnownDevices, Page } from "puppeteer";
+import status from "../../constants/status";
+import logger from "../../lib/logger";
 const iphoneSe = KnownDevices['iPhone SE']
 
 export const instagramFlow = async (req: Request, res: Response) => {
-  new Promise(async (_resolve, _reject) => {
+  const USERNAME = req.body.username
+  const PASSWORD = req.body.password
 
-    const USERNAME = req.body.username
-    const PASSWORD = req.body.password
+  if (!!USERNAME && !!PASSWORD) {
 
-    if (!!USERNAME && !!PASSWORD) {
+    res.status(status.HTTP_200_OK).json({
+      status: status.HTTP_200_OK,
+      message: 'OK',
+    });
 
+    new Promise(async () => {
       const browser = mainPage.browser()
       const page = await browser.newPage()
-
-      page.setDefaultTimeout(0)
 
       async function isInstagramLoggedIn(page: Page) {
         let isLoggedin = false
 
-        // to prevent throwing
-        await new Promise(async resolve => {
-          resolve(await page.waitForXPath(instagramSelectors.loginBtn))
+        await page.waitForXPath(instagramSelectors.loginBtn).then(async () => {
+          const isLoginBtnExist = await page.$x(instagramSelectors.loginBtn)
+          if (!!isLoginBtnExist && isLoginBtnExist.length > 0) {
+            // only the parent is clickable
+            const loginBtn = await isLoginBtnExist[0].$$('xpath/' + '..')
+            loginBtn[0].click()
+
+            // login process
+            await page.waitForSelector(instagramSelectors.emailField)
+            await page.type(instagramSelectors.emailField, USERNAME)
+            await page.type(instagramSelectors.passwordField, PASSWORD)
+
+            await Promise.all([
+              page.waitForNavigation({
+                waitUntil: 'domcontentloaded'
+              }),
+              page.click(instagramSelectors.loginSubmitBtn)
+            ])
+
+            await page.waitForSelector(instagramSelectors.securityCode).then(() => {
+
+              io.emit('instagram-security-code')
+
+            }).catch(() => {
+              logger.info('No security code needed')
+            })
+
+            // TODO: this doesn't triggered. propbably because there is no way
+            // back to this route.
+            io.on('instagram-security-code-input', async (code) => {
+              await page.type(instagramSelectors.securityCode, code)
+            })
+
+            await page.waitForSelector(instagramSelectors.mNewPost, {
+              timeout: 30000
+            })
+
+          }
+        }).catch(() => {
+          logger.info('Already Logged in')
         })
-
-        const isLoginBtnExist = await page.$x(instagramSelectors.loginBtn)
-        if (!!isLoginBtnExist && isLoginBtnExist.length > 0) {
-          // only the parent is clickable
-          const loginBtn = await isLoginBtnExist[0].$$('xpath/' + '..')
-          loginBtn[0].click()
-
-          // login process
-          await page.waitForSelector(instagramSelectors.emailField)
-          await page.type(instagramSelectors.emailField, USERNAME)
-          await page.type(instagramSelectors.passwordField, PASSWORD)
-
-          await Promise.all([
-            page.waitForNavigation({
-              waitUntil: 'domcontentloaded'
-            }),
-            page.click(instagramSelectors.loginSubmitBtn)
-          ])
-
-          io.emit('instagram-security-code')
-
-          io.on('instagram-security-code-input', async (code) => {
-            // TODO: start testing this
-            await page.waitForSelector(instagramSelectors.securityCode)
-            await page.type(instagramSelectors.securityCode, code)
-            console.log(code)
-          })
-
-          await page.waitForSelector(instagramSelectors.mNewPost, {
-            timeout: 30000
-          })
-
-        }
 
         isLoggedin = true
 
@@ -77,10 +85,14 @@ export const instagramFlow = async (req: Request, res: Response) => {
 
       } catch (error: any) {
         const errorMsg = error?.message as string
-        console.log(errorMsg)
+        logger.error(errorMsg)
       }
-    }
-  })
+    })
 
-  res.status(200).json({ message: 'OK' });
+  } else {
+    res.status(status.HTTP_400_BAD_REQUEST).json({
+      status: status.HTTP_400_BAD_REQUEST,
+      message: 'No Username Provided',
+    });
+  }
 };
