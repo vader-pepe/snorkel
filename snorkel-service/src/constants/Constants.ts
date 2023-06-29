0b5260992faba13666166b4d932e0dd1d667ca64
name's already taken <93415755+chadwithz@users.reply.github.com>
2023-06-29 21:02:27 +0700

    feat: move to pc

---
+++ snorkel-service/src/constants/Constants.ts
@@ -0,0 +1,21 @@
+import { PuppeteerLaunchOptions, ConnectOptions } from "puppeteer";
+
+type Options = PuppeteerLaunchOptions & ConnectOptions
+
+export const DefaultOptions = {
+  puppeteer: {
+    headless: true,
+    defaultViewport: null,
+  } as Options,
+  webVersion: '2.2322.15',
+  webVersionCache: {
+    type: 'local',
+  },
+  authTimeoutMs: 0,
+  takeoverOnConflict: false,
+  takeoverTimeoutMs: 0,
+  userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36',
+  ffmpegPath: 'ffmpeg',
+  bypassCSP: false,
+  proxyAuthentication: undefined,
+};
--- snorkel-service/src/index.ts
+++ snorkel-service/src/index.ts
@@ -1,45 +1,5 @@
-import app from "./app"
-import config from "./config"
-import { AddressInfo } from 'net'
-import http from "http"
-import { Server } from "socket.io"
-import { Page } from "puppeteer"
-import puppeteerInstance from "@/lib/puppeteer-instance"
-import logger from "@/lib/logger"
-import socketInstance from "@/lib/socket-instance"
 
-const httpServer = http.createServer(app)
-export let page: Page
 
-export const io = new Server(httpServer, {
-  cors: {
-    origin: 'http://localhost:3000',
-  }
-})
-
-const PORT = process.env.PORT || config.port;
-
-puppeteerInstance().then((mainPage) => {
-  page = mainPage
-  logger.info('Browser initialized')
-  // close the extra page
-  page.close()
-  const server = httpServer.listen(PORT, async () => {
-    const { port } = server.address() as AddressInfo
-    console.log('server is running on port', port);
-  });
-
-}).catch(async (error) => {
-  const errorMsg = error.message as string
-  logger.error(errorMsg)
-})
-
-io.on("connection", async (socket) => {
-  console.log("user connected")
-
-  socketInstance(socket)
-
-  socket.on('disconnect', async () => {
-    console.log('user disconnected');
-  });
-})
+export default {
+  Client: 
+}
