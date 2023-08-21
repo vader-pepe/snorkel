75f6e54346d5743e1d404baaee1db148b3d05f9d
chadwithz <kepoya@email.com>
2023-08-21 18:16:21 +0700

    fix: facebook flow

--- engine/src/InterfaceController.ts
+++ engine/src/InterfaceController.ts
@@ -1,11 +1,21 @@
-import { KnownDevices, Page } from "puppeteer-core";
+import { KnownDevices, Page, Protocol } from "puppeteer-core";
+import { readFile } from "fs/promises";
+import path from "path";
+
 import { FacebookController } from "./facebook/Controller";
 import { InstagramController } from "./instagram/Controller";
 import { TwitterController } from "./twitter/Controller";
 import config from "./config";
+const root = path.resolve('./src')
 
 const pixel5 = KnownDevices['Pixel 5']
 const { platforms } = config
+
+interface CookieCollection {
+  facebook: Array<Protocol.Network.CookieParam>
+  instagram: Array<Protocol.Network.CookieParam>
+  twitter: Array<Protocol.Network.CookieParam>
+}
 
 export type IPlatfroms =
   'https://www.instagram.com/' |
@@ -28,28 +38,37 @@ class InterfaceController {
 
     await Promise.all(platforms.map(async platform => {
       const page = await browser.newPage()
-      if (platform !== 'https://www.instagram.com/') {
-        await page.setUserAgent('Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36')
-        await page.emulate(pixel5)
-      } else {
-        await page.setUserAgent('Mozilla/5.0 (Linux; Android 4.3; Nexus 7 Build/JSS15Q) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36')
-      }
-      await page.goto(platform, {
-        waitUntil: 'domcontentloaded'
-      })
+      const cookieRaw = await readFile(`${root}/users.json`, 'utf8').catch(() => { throw new Error('No Cookie!') })
+      const cookie = JSON.parse(cookieRaw) as unknown as CookieCollection
 
       switch (platform) {
         case 'https://www.facebook.com/':
+          await page.setCookie(...cookie.facebook)
+          await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36')
+          await page.goto(platform, {
+            waitUntil: 'load'
+          })
           this.facebook = new FacebookController(page)
           console.log('facebook instance created')
           break;
 
         case 'https://www.instagram.com/':
+          await page.setCookie(...cookie.instagram)
+          await page.setUserAgent('Mozilla/5.0 (Linux; Android 4.3; Nexus 7 Build/JSS15Q) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36')
           this.instagram = new InstagramController(page)
+          await page.goto(platform, {
+            waitUntil: 'load'
+          })
           console.log('instagram instance created')
           break;
 
         case 'https://twitter.com/':
+          await page.setCookie(...cookie.twitter)
+          await page.setUserAgent('Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36')
+          await page.emulate(pixel5)
+          await page.goto(platform, {
+            waitUntil: 'load'
+          })
           this.twitter = new TwitterController(page)
           console.log('twitter instance created')
           break;
--- engine/src/constants/facebook/index.ts
+++ engine/src/constants/facebook/index.ts
@@ -14,35 +14,35 @@ export const facebookSelectors = {
   mNotNow: `a[role="button"][target="_self"][data-sigil="touchable"]`,
   mNewPost: `div[role="button"]`,
   mStatusField: `textarea[aria-label="What's on your mind?"][data-sigil="composer-textarea m-textarea-input"]`,
-  mStatusFieldXpath: `//div[contains(text(),"What's on your mind?")]`,
+  statusFieldXpath: `//span[contains(text(),"What's on your mind")]`,
   mStatusFieldOnFill: `//button[contains(text(), "What's on your mind?")]`,
   mPhotoBtn: `button[data-sigil="touchable hidden-button photo-button"][type="button"]`,
-  mNewPhotosPost: `//div[@role="button"][div[div[text()[contains(
+  newPhotoOrVideoPost: '[aria-label="Photo/video"]',
+  mediaInput: `//div[@role="button"][div[div[div[div[div[span[
+  contains(
     translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),
-    'photo'
-  )]]]]`,
+    'add photos'
+  )
+]]]]]]]`,
   skipNContinue: `//span[text()[contains(
     translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),
     "Skip and continue posting"
   )]]`,
   // =============================== V2 =====================================
-  whatsOnYourmind: `//div[@role="button"][@aria-label[contains(
-    translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),
-    'post'
-  )]]`,
+  whatsOnYourmind: `//div[@role="button"][div[span[contains(text(),"What's on your mind")]]]`,
   captionSpawner: `//button[contains(text(),"What's on your mind?")]`,
-  captionTextarea: `//div[@class="textbox-container with-mentions"]//textarea`,
-  submitPostBtn: `//button[span[contains(text(),"POST")]]`,
+  captionTextarea: `//*[@data-lexical-editor="true"][@contenteditable][@aria-label[
+  contains(
+    translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),
+    "what's on your mind"
+  )
+]]`,
+  submitPostBtn: `div[aria-label="Post"]`,
   mNewVideoPost: `//div[@role="button"][div[div[text()[contains(
     translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),
     'video'
   )]]]]`,
-  postedToast: `//div[@class="toast-label"][text()[
-  contains(
-    translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),
-    'posted'
-  )
-]]`,
+  postedToast: `[aria-atomic="true"][aria-live="polite"][role="alert"]`,
   timelines: `//div[@class="m"][@data-tracking-duration-id][@data-type="container"][div[div[@data-comp-id]]]`,
   // desktop!
   posts: `//div[div[div[div[div[div[@aria-posinset]]]]]]`,
